3baec31b049170897a50792f0bf514840fd82422
Ali Albaali <alialbaali@protonmail.ch>
2020-06-13 01:45:51 +0300

    Remove userEmail from Primary keys and assign a unique index for it

--- local/src/main/kotlin/com/noto/database/model/Users.kt
+++ local/src/main/kotlin/com/noto/database/model/Users.kt
@@ -6,13 +6,13 @@ import org.joda.time.DateTime
 
 object Users : Table(name = "users") {
     override val primaryKey: PrimaryKey
-        get() = PrimaryKey(userId, userEmail)
+        get() = PrimaryKey(userId, name = "user_id")
 
     val userId = long("user_id").autoIncrement()
 
     val userDisplayName = varchar("user_display_name", 32)
 
-    val userEmail = varchar("user_email", 64)
+    val userEmail = varchar("user_email", 64).uniqueIndex()
 
     val userPassword = varchar("user_password", 256)
 
